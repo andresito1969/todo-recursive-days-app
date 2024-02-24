58d326c5d868c79afe7b672d05d55da7dfd81793
Andrés <ernestoandresrosillo@gmail.com>
2024-02-24 17:07:56 +0100

    [CLEAN] CODE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,69 +14,10 @@ use Exception;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     private $userRepository;
 
     public function __construct(UserRepository $userRepository) {
         $this->userRepository = $userRepository;
-    }
-
-    public function index()
-    {
-        $userList = $this->userRepository->getAllUsers();
-
-        return new UserCollection($userList);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        return $this->userRepository->getUserById($id);
-        // return new UserCollection($user);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 
     public function login(Request $request) {
