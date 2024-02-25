67c5e007813d60e3f2ea9a190ca914058888ae5f
Andrés <ernestoandresrosillo@gmail.com>
2024-02-25 02:19:28 +0100

    [CORRECTION] CORRECTION OF REDIRECTION IN APP, USING 100% VUE ROUTER IN EVERY REQUEST :D

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo App</title>
     <link rel="icon" type="image/x-icon" href="check-icon.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     @vite('frontend/js/main.js')
 </head>
 <body>
