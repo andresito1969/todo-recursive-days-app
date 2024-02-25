677bc0eba530d2f7b4a3cfb20857cbff23b81a1c
Andrés <ernestoandresrosillo@gmail.com>
2024-02-25 23:54:41 +0100

    [DEVELOP] PROTECTED ROUTES ADDED MORE SECURITY, ONLY ALLOW ROUTES THAT ARE REGISTERED, SO WE CAN'T ACCESS TO INVETED ROUTES THROUGH THE ADDRESS BAR

--- frontend/js/router/index.js
+++ frontend/js/router/index.js
@@ -30,19 +30,36 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const isAuthenticated = JSON.parse(sessionStorage.getItem('userData'));
+  const isValidRoute = checkValidRoute(to);
 
-  // if user is not auth and route requires auth, redirect to login
-  if (to?.meta?.requiresAuth && !isAuthenticated) {
+  // if user is not auth and route requires auth or is not valid route, redirect to login
+  if ((to?.meta?.requiresAuth && !isAuthenticated) || !isValidRoute) {
+    console.log("guard 1");
     next('/login'); 
   } 
 
   // if user is auth and route requires to exclusive not auth, stay in same page
   if(!to?.meta?.requiresAuth && isAuthenticated) {
+    console.log("guard 2")
     next(from.path)
   }
 
   next();
-  
 });
+
+const checkValidRoute = (to) => {
+  let i = 0;
+  let isValidRoute = false;
+  console.log(router);
+  while(i < router.options.routes.length && !isValidRoute) {
+    const routeOption = router.options.routes[i];
+    if(routeOption.path === to.path) {
+      isValidRoute = true;
+    }
+    i++;
+  }
+
+  return isValidRoute;
+}
 
 export default router
